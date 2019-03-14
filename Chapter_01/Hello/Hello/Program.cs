4490edd54cb8eccc46b94ec146327c6180e9b197
Kimberly Henry <Kimberly.Henry@outlook.com>
2019-03-14 03:52:15 -0400

    Added comments to program

--- Chapter_01/Hello/Hello/Program.cs
+++ Chapter_01/Hello/Hello/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿///Filename:  Hello.cs
+///Written By:  Kimberly Henry
+///Written on March 13, 2019
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +13,10 @@ namespace Hello
     class Program
     {
         static void Main(string[] args)
+            /*This program demonstrates the use of
+             * the WriteLine() metho to display the
+             * message Hello, world!*/
+
         {
             Console.WriteLine("Hello world!");
             Console.ReadLine();
