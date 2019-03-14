2bd1625d5a786bf14003b6199503c109509e1b4d
Kimberly Henry <Kimberly.Henry@outlook.com>
2019-03-14 17:56:27 -0400

    Quotation Applicaton

---
+++ Chapter_01/Quotation/Quotation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gone with the Wind");
+            Console.WriteLine("Clark Gable");
+            Console.WriteLine("Frankly my dear, I don't give a dam");
+            Console.ReadLine();
+
+        }
+    }
+}
