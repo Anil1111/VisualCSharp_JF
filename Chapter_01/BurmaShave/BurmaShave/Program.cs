f0a0e518798c5a5d0d12727b152acc9864d4c0fd
Kimberly Henry <Kimberly.Henry@outlook.com>
2019-03-14 19:08:27 -0400

    BurmaShave Project

---
+++ Chapter_01/BurmaShave/BurmaShave/Program.cs
@@ -0,0 +1,24 @@
+﻿///Filename:  BurmaShave.cs
+///Written By:  Kimberly Henry
+///Written on March 14, 2019
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurmaShave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Approached a crossng" +
+                "\nWithout looking" +
+                "\nWho will eat" +
+                "\nHis wife's cooking.");
+            Console.ReadLine();
+        }
+    }
+}
