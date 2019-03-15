a356a1e1e53a245c9b5fafc508ace4df83d113fa
Kimberly Henry <Kimberly.Henry@outlook.com>
2019-03-14 23:46:14 -0400

    Tree Project

---
+++ Chapter_01/Tree/Tree/Program.cs
@@ -0,0 +1,25 @@
+﻿///Filename:  Tree.cs
+///Written By:  Kimberly Henry
+///Written on March 14, 2019
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("    X");
+            Console.WriteLine("   X0X");
+            Console.WriteLine("  X0X0X");
+            Console.WriteLine(" X0X0X0X");
+            Console.WriteLine("    X");
+            Console.ReadLine();
+        }
+    }
+}
