61e36c414450862c030cae01168e4441375980dc
Kimberly Henry <Kimberly.Henry@outlook.com>
2019-03-15 01:37:23 -0400

    Initials Project

---
+++ Chapter_01/Initials/Initials/Program.cs
@@ -0,0 +1,29 @@
+﻿///Filename:  Initials.cs
+///Written By:  Kimberly Henry
+///Written on March 15, 2019
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Initials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("KK         KK       HH           HH");
+            Console.WriteLine("KK       KK         HH           HH");
+            Console.WriteLine("KK     KK           HH           HH");
+            Console.WriteLine("KK   KK             HHHHHHHHHHHHHHH");
+            Console.WriteLine("KK K                HHHHHHHHHHHHHHH");
+            Console.WriteLine("KK   KK             HH           HH");
+            Console.WriteLine("KK     KK           HH           HH");
+            Console.WriteLine("KK       KK         HH           HH");
+            Console.WriteLine("KK         KK       HH           HH");
+            Console.ReadLine();
+        }
+    }
+}
