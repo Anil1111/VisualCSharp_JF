cd6b4c4ea89dc776a22dd419ba698586e21a5a6e
Kimberly Henry <Kimberly.Henry@outlook.com>
2019-03-15 03:09:06 -0400

    FixDebugOne3
    Converted from Java Programming.

---
+++ Chapter_01/FixDebugOne3/FixDebugOne3/Program.cs
@@ -0,0 +1,19 @@
+﻿///Filename:  FixDebugOne3.cs
+///Written By:  Kimberly Henry
+///Written on March 15, 2019
+
+using System;
+
+namespace FixDebugOne3
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Over the river");
+            Console.WriteLine("and through the woods");
+            Console.WriteLine("to Grandmother's house we go");
+            Console.ReadLine();
+        }
+    }
+}
