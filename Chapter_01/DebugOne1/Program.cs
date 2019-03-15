c9993b68d5bfc2f37499ba79819e26cd2e1566ba
Kimberly Henry <Kimberly.Henry@outlook.com>
2019-03-15 02:35:53 -0400

    FixDebugOne1 Project
    Add Java Programming Debugging Exercises to convert and debug in C#.

---
+++ Chapter_01/DebugOne1/Program.cs
@@ -0,0 +1,18 @@
+﻿///Filename:  FixDebugOne1.cs
+///Written By:  Kimberly Henry
+///Written on March 13, 2019
+
+using System;
+
+namespace FixDebugOne1
+{
+    class Program
+    {
+        /* This program displays a greeting */
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.ReadLine();
+        }
+    }
+}
