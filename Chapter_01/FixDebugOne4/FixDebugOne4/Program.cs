df8a15b5e9a1cfc857b6f037038ff54e1ec153b6
Kimberly Henry <Kimberly.Henry@outlook.com>
2019-03-15 15:58:01 -0400

    FixDebugOne4
    Converted and debugged from Java Programming.

---
+++ Chapter_01/FixDebugOne4/FixDebugOne4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FixDebugOne4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This should be my first GUI Application");
+            Console.ReadLine();
+        }
+    }
+}
