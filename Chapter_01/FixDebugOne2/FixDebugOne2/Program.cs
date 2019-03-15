1a69fbcaea462763595d14fe9de01b82342908d3
Kimberly Henry <Kimberly.Henry@outlook.com>
2019-03-15 03:00:20 -0400

    FixDebugOne2 Project
    Converted from Java Program

---
+++ Chapter_01/FixDebugOne2/FixDebugOne2/Program.cs
@@ -0,0 +1,22 @@
+﻿///Filename:  Hello.cs
+///Written By:  Kimberly Henry
+///Written on March 13, 2019
+
+using System;
+
+namespace FixDebugOne2
+{
+    class Program
+    {
+        /* This program displays some output*/
+        public static void Main()
+        {
+            Console.WriteLine("C# programming is fun.");
+            Console.WriteLine("Getting a program to work");
+            Console.WriteLine("can be a challenge,");
+            Console.WriteLine("but when everything works correctly,");
+            Console.WriteLine("it's very satisfying");
+            Console.ReadLine();
+        }
+    }
+}
