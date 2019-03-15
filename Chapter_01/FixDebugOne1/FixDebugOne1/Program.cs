98fe5381adf128e48988ef05cf8bf6126a1a6535
Kimberly Henry <Kimberly.Henry@outlook.com>
2019-03-15 04:25:05 -0400

    FixDebugOne1
    Corrected errors.

---
+++ Chapter_01/FixDebugOne1/FixDebugOne1/Program.cs
@@ -0,0 +1,18 @@
+﻿///Filename:  FixDebugOne1
+///Written By:  Kimberly Henry
+///Written on March 15, 2019
+
+using System;
+
+namespace FixDebugOne1
+{
+    /* This program displays a greeting */
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello");
+            Console.ReadLine();
+        }
+    }
+}
