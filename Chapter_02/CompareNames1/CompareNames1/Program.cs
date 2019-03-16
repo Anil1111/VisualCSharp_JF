a7d2e7b57e915cb1f671cf09bf2ff3d59fc0b395
Kimberly Henry <Kimberly.Henry@outlook.com>
2019-03-16 03:04:12 -0400

    CompareNames1

---
+++ Chapter_02/CompareNames1/CompareNames1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CompareNames1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name1 = "Amy";
+            string name2 = "Amy";
+            string name3 = "Matthew";
+            Console.WriteLine("Compare {0} to {1}: {2}", name1, name2, name1 == name2);
+            Console.WriteLine("Compare {0} to {1}:  {2}", name1, name3, name1 == name3);
+            Console.ReadLine();
+        }
+    }
+}
