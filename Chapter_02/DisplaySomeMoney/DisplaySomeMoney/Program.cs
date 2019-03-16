da5bda3258b2f8d1115ccf61067c5b751d14d293
Kimberly Henry <Kimberly.Henry@outlook.com>
2019-03-15 23:01:12 -0400

    Figure 2-1
    In text example

---
+++ Chapter_02/DisplaySomeMoney/DisplaySomeMoney/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+using System;
+
+namespace DisplaySomeMoney
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double someMoney = 39.45;
+            Console.WriteLine(someMoney);
+            Console.ReadLine();
+        }
+    }
+}
