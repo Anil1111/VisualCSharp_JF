6ef5b56d7311b4a0c3ff531ffed9ca204a6910c0
Kimberly Henry <Kimberly.Henry@outlook.com>
2019-03-16 16:55:23 -0400

    DemoVariables

---
+++ Chapter_02/DemoVariables/DemoVariables/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DemoVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long anInt = -123456789000;
+            uint anUnsignedInt = 567;
+
+            Console.WriteLine("The long is {0} and the unsigned int is {1}", anInt, anUnsignedInt);
+            Console.ReadLine();
+        }
+    }
+}
