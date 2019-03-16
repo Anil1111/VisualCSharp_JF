e79cd1ee0e1ef457002df656ed900e311ca67fd1
Kimberly Henry <Kimberly.Henry@outlook.com>
2019-03-15 23:49:46 -0400

    Figure 2-5

---
+++ Chapter_02/DisplaySomeMoney3/DisplaySomeMoney3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DisplaySomeMoney3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double someMoney = 39.45;
+            Console.WriteLine("The money is ${0} exactly", someMoney);
+            Console.ReadLine();
+        }
+    }
+}
