70d9d8658152753002337324b5571c0cc13f9542
Kimberly Henry <Kimberly.Henry@outlook.com>
2019-03-17 00:44:29 -0400

    DemoEscapeSequences

---
+++ Chapter_02/DemoEscapeSequences/DemoEscapeSequences/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DemoEscapeSequences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This line\tcontains \ttwo tabs");
+            Console.WriteLine("This statement\ncontains a new line");
+            Console.WriteLine("This statement sounds three alerts\a\a\a");
+            Console.ReadLine();
+
+        }
+    }
+}
