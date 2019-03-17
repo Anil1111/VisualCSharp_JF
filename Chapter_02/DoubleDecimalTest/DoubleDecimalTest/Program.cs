d40842820164feb68bd98abc167f7a77a1f6756e
Kimberly Henry <Kimberly.Henry@outlook.com>
2019-03-17 15:45:42 -0400

    DoubleDecimalTest

---
+++ Chapter_02/DoubleDecimalTest/DoubleDecimalTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DoubleDecimalTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float test1 = 123456997890; //code runs and displays number with error.
+            double test2 = 987654323456789;
+            Console.WriteLine("Test1 value is {0}", test1);
+            Console.WriteLine("Test2 value is {0}", test2);
+            Console.ReadLine();
+
+        }
+    }
+}
