c1a812ebe7604aea6700e2380d7e3074e444f68d
Kimberly Henry <Kimberly.Henry@outlook.com>
2019-03-17 01:52:28 -0400

    InteractiveAddition

---
+++ Chapter_02/InteractiveAddition/InteractiveAddition/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace InteractiveAddition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, firstString, secondString;
+            int first, second, sum;
+
+            Console.Write("Enter your name...");
+            name = Console.ReadLine();
+
+            Console.Write("Hello {0}!\nEnter the first integer... ", name);
+            firstString = Console.ReadLine();
+            first = Convert.ToInt32(firstString);
+
+            Console.Write("Enter the second integer...");
+            secondString = Console.ReadLine();
+            second = Convert.ToInt32(secondString);
+
+            sum = first + second;
+            Console.WriteLine("{0}, the sum of {1} and {2} is {3}.", name, first, second, sum);
+            Console.ReadLine();
+        }
+    }
+}
