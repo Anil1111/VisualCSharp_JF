e690f6d535900258c6f9e052c1fd55a05e2f9cea
Kimberly Henry <Kimberly.Henry@outlook.com>
2019-03-21 22:54:50 -0400

    EggsInteractive

---
+++ Chapter_02/HoursAndMinutesInteractive/Program.cs
@@ -0,0 +1,25 @@
+﻿///Modify the Eggs program in Exercise 10 so it prompts the user for and 
+///accepts a number of eggs for each chicken. Save the program as 
+///EggsInteractive.cs.  (Page 100).
+
+using System;
+
+namespace EggsInteractive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int DOZEN = 12;
+           
+            Console.WriteLine("How many eggs would you like?");
+            int eggs = Convert.ToInt32(Console.ReadLine());
+
+            int dozenEggs = eggs / DOZEN;
+            int singleEggs = eggs % DOZEN;
+            Console.WriteLine("A total of {0} eggs are required.", eggs);
+            Console.WriteLine("This calculates to {0} dozen with a remainding {1} single eggs.", dozenEggs, singleEggs);
+            Console.ReadLine();
+        }
+    }
+}
