ee376ebd4cf663dd9fd6fbe89291cb26b077ad41
Kimberly Henry <Kimberly.Henry@outlook.com>
2019-03-22 15:55:39 -0400

    Test

---
+++ Chapter_02/Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int TEST = 5;
+            int test1 = 65;
+            int test2 = 78;
+            int test3 = 99;
+            int test4 = 100;
+            int test5 = 98;
+            double avgScore = (test1 + test2 + test3 + test4 + test5) / TEST;
+            Console.WriteLine("The average score is {0:0.00} avg", avgScore);
+            Console.ReadKey();
+        }
+    }
+}
