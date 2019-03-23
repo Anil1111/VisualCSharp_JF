530a9d8859b8a8beeb0bad83058bf0c5d4a2a1b9
Kimberly Henry <Kimberly.Henry@outlook.com>
2019-03-22 21:05:36 -0400

    TwoFriends

---
+++ Chapter_02/TwoFriends/Program.cs
@@ -0,0 +1,20 @@
+﻿///Write a program that declares two variables to hold the names of two of 
+///your friends, and assign a value to each variable. Display the result of
+///using the String.Compare() method with your friends’ names. Save the 
+///program as TwoFriends.cs.  (Page 100).
+
+using System;
+
+namespace TwoFriends
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstFriend = "Felicia";
+            string secondFriend = "Arthur";
+            Console.WriteLine("Compare {0} to {1}: {2}", firstFriend, secondFriend, firstFriend.CompareTo(secondFriend));
+            Console.ReadLine();
+        }
+    }
+}
