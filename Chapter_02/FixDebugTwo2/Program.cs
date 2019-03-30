92512da9df733747fb554aea517aa266b3ba708b
Kimberly Henry <Kimberly.Henry@outlook.com>
2019-03-29 20:41:25 -0400

    FixDebugTwo2

---
+++ Chapter_02/FixDebugTwo2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FixDebugTwo2
+{
+	class Program
+	{
+		static void Main (string [] args)
+		{
+			int a, b;
+			a = 7;
+			b = 4;
+			Console.WriteLine ("The sum is {0}", (a + b));
+			Console.WriteLine ("The difference is {0}", + (a - b));
+			Console.WriteLine ("The product is {0}", + (a * b));
+			Console.ReadLine();
+		}
+	}
+}
