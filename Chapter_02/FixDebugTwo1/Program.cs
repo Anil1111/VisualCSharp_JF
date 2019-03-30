8cabc7cd4d49aa850ed2ef9abadf4f5d138d1b43
Kimberly Henry <Kimberly.Henry@outlook.com>
2019-03-29 20:14:10 -0400

    FixDebugTwo1

---
+++ Chapter_02/FixDebugTwo1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FixDebugTwo1
+{
+	class Program
+	{
+		static void Main (string [] args)
+		{
+			double oneInt = 315.0;
+			oneInt = Convert.ToInt32(oneInt);
+			double oneDouble = 12.4;
+			char oneChar = 'A';
+			Console.WriteLine ("The int is {0}", oneInt);
+			Console.WriteLine ("The double is {0}", oneDouble);
+			Console.WriteLine ("The char is {0}", oneChar);
+			Console.ReadLine();
+		}
+	}
+}
