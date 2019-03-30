630972fc2393c2a9cf257d15f7a05a9368046d9d
Kimberly Henry <Kimberly.Henry@outlook.com>
2019-03-29 21:05:06 -0400

    FixDebugTwo3

---
+++ Chapter_02/FixDebugTwo3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FixDebugTwo3
+{
+	class Program
+	{
+		static void Main (string [] args)
+		{
+			int a = 99, b = 8, result;
+			long c = 7777777777777;
+			result = a % b;
+			Console.WriteLine("Divide {0} by {1} is {2} ", a, b, (a / b));
+			Console.WriteLine("remainder is {0} ", + result);
+			Console.WriteLine("c is a very large number: {0} ", c);
+			Console.ReadLine();
+		}
+	}
+}
