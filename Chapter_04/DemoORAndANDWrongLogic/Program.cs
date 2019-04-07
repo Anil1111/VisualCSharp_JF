dd359a5dac4d6e42538ea8053c06cb19e4806f35
Kimberly Henry <Kimberly.Henry@outlook.com>
2019-04-07 01:32:43 -0400

    DemoORAndAND

---
+++ Chapter_04/DemoORAndANDWrongLogic/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DemoORAndAND
+{
+	class Program
+	{
+		static void Main (string [] args)
+		{
+			const int ZONE1  = 1, ZONE2 = 2;
+			const int LOWQUANTITY = 10;
+			string inputString;
+			int quantity;
+			int deliveryZone;
+			Console.WriteLine("Delivery is free for zone {0} or {1} ", ZONE1, ZONE2);
+			Console.WriteLine("when the number of boxes is less than {0}", LOWQUANTITY);
+			Console.WriteLine("Enter deivery zone ");
+			inputString = Console.ReadLine();
+			deliveryZone = Convert.ToInt32 (inputString);
+			Console.WriteLine("Enter the number of boxes in the shipment");
+			inputString = Console.ReadLine();
+			quantity = Convert.ToInt32(inputString);
+			if((deliveryZone == ZONE1 || deliveryZone == ZONE2) && quantity < LOWQUANTITY)
+				Console.WriteLine("Delivery is free");
+			else
+				Console.WriteLine("A delivery charge applies");
+		}
+	}
+}
