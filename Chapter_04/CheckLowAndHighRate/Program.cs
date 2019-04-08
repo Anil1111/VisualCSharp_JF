7a76059066d413b77c59d10354fc8f08c7e04400
Kimberly Henry <Kimberly.Henry@outlook.com>
2019-04-08 00:30:06 -0400

    CheckLowAndHighRate

---
+++ Chapter_04/CheckLowAndHighRate/Program.cs
@@ -0,0 +1,26 @@
+﻿///Write a console-based program that prompts a user for an hourly pay rate. 
+///If the value entered is less than $7.50 or greater than $49.99, display 
+///an error message; otherwise, display a message indicating that the rate 
+///is okay. Save the program as CheckLowAndHighRate.cs.
+
+using System;
+
+namespace CheckLowAndHighRate
+{
+	class Program
+	{
+		static void Main (string [] args)
+		{
+			const double MIN_RATE = 7.50;
+			const double MAX_RATE = 49.99;
+			Console.Write("Enter the hourly pay rate:  ");
+			string payRate = Console.ReadLine();
+			double hourlyPayRate = Convert.ToDouble(payRate);
+			if(hourlyPayRate < MIN_RATE || hourlyPayRate > MAX_RATE)
+				Console.WriteLine("Pay rate is invalid");
+			else
+				Console.WriteLine("Pay rate is valid.");
+			Console.ReadLine();
+		}
+	}
+}
