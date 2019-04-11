7ac9b923be358c676a9d5445cb5402ccff5d6841
Kimberly Henry <Kimberly.Henry@outlook.com>
2019-04-11 00:45:54 -0400

    SecondChancePayRateGUI

---
+++ Chapter_04/Twitter/Twitter/Program.cs
@@ -0,0 +1,30 @@
+﻿///Write a console-based program that accepts a user’s message and determines whether
+///it is short enough for a social networking service that does not accept messages 
+///of more than 140 characters. Save the program as Twitter.cs.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Twitter
+{
+	class Program
+	{
+		static void Main(string [] args)
+		{
+			int characters; 
+			string message;
+			Console.Write("Enter your message");
+			message = Console.ReadLine();
+			characters = message.Length;
+			if(characters > 140)
+				Console.WriteLine("Message is too long.");
+			else
+				Console.WriteLine("Message is valid.");
+			Console.ReadLine();
+
+		}
+	}
+}
